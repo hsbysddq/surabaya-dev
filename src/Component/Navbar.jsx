cfb6c3a69d90d67757dd58b5a4674133a9be4685
hsbysddq <88543652+hsbysddq@users.noreply.github.com>
2023-01-19 22:10:18 +0700

    change logo

--- src/Component/Navbar.jsx
+++ src/Component/Navbar.jsx
@@ -1,27 +1,26 @@
+import logo from "../assets/t.png"
+
 function Navbar() {
   return (
-    <nav className="flex justify-between text-white pt-[1.8%]">
-      <div className="logo p-4">
-        <a
-          href="/"
-          className="site-tittle flex text-grey text-2xl font-inter font-extrabold"
-        >
-          Surabaya Js
+    <nav className="flex justify-center justify-between text-white pt-[1.8%]">
+      <div className="logo">
+        <a href="/" className="flex">
+          <img src={logo} alt="" className="logo" />
         </a>
       </div>
 
       <div className="list">
         <ul className="flex list-none gap-4 text-lightGrey text-xl font-inter font-extrabold">
-          <li className="p-4">
+          <li className="p-5">
             <a href="/">About</a>
           </li>
-          <li className="p-4">
+          <li className="p-5">
             <a href="/">Event</a>
           </li>
-          <li className="p-4">
+          <li className="p-5">
             <a href="/">Career</a>
           </li>
-          <li className="p-3">
+          <li className="p-4">
             <button className="text-xs outline rounded-sm px-4 py-2">
               Resume
             </button>
