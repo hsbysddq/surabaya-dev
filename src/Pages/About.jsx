eb107d32daa45eefe1669e3a911b18ca6e6b82f0
hsbysddq <88543652+hsbysddq@users.noreply.github.com>
2023-01-20 23:48:19 +0700

    edit about

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -2,7 +2,7 @@ function About() {
   return (
     <div className="bg-cover bg-center bg-about h-screen w-screen">
       <h1 className="text-lightGrey font-inter text-4xl font-extrabold pt-[500px]">
-        Hello
+        {" "}
       </h1>
     </div>
   )
