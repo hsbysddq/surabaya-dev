46c9571411e48520f2c414033c991b7e4cd57a8c
hsbysddq <88543652+hsbysddq@users.noreply.github.com>
2023-01-20 19:15:56 +0700

    edit tagline

--- src/Component/Tagline.jsx
+++ src/Component/Tagline.jsx
@@ -1,15 +1,17 @@
 function Tagline() {
   return (
-    <div className="w-[55%] text-lightGrey font-inter">
-      <h1 className="pt-[100px] text-4xl font-extrabold">
-        Hallo Para Developer
-      </h1>
-      <p className="pt-[20px] text-xl">
+    <div className="w-[50%] text-lightGrey font-inter">
+      <h1 className="pt-[100px] text-4xl font-extrabold">Hello Developer</h1>
+      <p className="pt-[20px] pb-6 text-xl">
         Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ullam animi
         consequatur illum itaque amet laboriosam maiores, voluptas aliquid nulla
         nemo excepturi harum laudantium ex natus obcaecati accusantium dolores
         quibusdam qui.
       </p>
+
+      <button className="text-lightGrey outline rounded-md px-6 py-3">
+        Gabung Sekarang!
+      </button>
     </div>
   )
 }
