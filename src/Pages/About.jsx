d31c3337a7b0fd692fd644eadded0d0c96008c21
hsbysddq <88543652+hsbysddq@users.noreply.github.com>
2023-01-21 15:54:50 +0700

    update about

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -1,12 +1,30 @@
 function About() {
   return (
     <div className="justify-center about grid grid-cols-3 text-lightGrey font-inter text-2xl">
-      <h1 className="col-span-3">
+      <h1 className="col-span-3 mb-8">
         Surabaya Dev bertuuan menjadi wadah talenta teknologi dengan
         memanfaatkan potensinya dengan meningkatkan skill tech di bidang
         programming maupun digital teknologi agar dapat saling bersinergi
         berkolaborasi.
       </h1>
+
+      <p>Wadah Belajar</p>
+      <p>Koneksi</p>
+      <p>Berbagi Ilmu</p>
+
+      <p className="mt-8 w-[80%]">
+        Menjadi wadah belajar pengiat IT untuk bisa terus update mengenai
+        techindustry saat ini.
+      </p>
+
+      <p className="mt-8 w-[80%]">
+        Memperluas relasi melalui komunitas untuk menjalin kebermanfaatan.
+      </p>
+
+      <p className="mt-8 w-[80%]">
+        Komunitas adalah wadah yang tepat untuk saling berbagi ilmu, pengalaman
+        maupun wawasan tentang update teknologi.
+      </p>
     </div>
   )
 }
