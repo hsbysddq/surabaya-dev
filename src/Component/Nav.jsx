3b99726fced042c949d881d479f1b39c3045c096
hsbysddq <88543652+hsbysddq@users.noreply.github.com>
2023-01-27 15:19:17 +0700

    Fix Responsive

--- src/Component/Nav.jsx
+++ src/Component/Nav.jsx
@@ -3,22 +3,27 @@ import Logo from "../assets/logo.png"
 const Nav = () => {
   const Links = [{ name: "About" }, { name: "Event" }, { name: "Career" }]
   return (
-    <nav className="flex items-center justify-between pt-5">
-      <img src={Logo} alt="" className="w-10 ml-7" />
-      <ul>
+    <nav className="flex items-center justify-between pt-8">
+      <img src={Logo} alt="" className="w-44" />
+      <ul className="flex">
         {Links.map((link, index) => (
           <li
             key={index}
-            className="md:inline-block md:ml-10 ml-5 border-b-2 border-transparent hover:border-white duration-300"
+            className="md:inline-block md:ml-10 ml-5 border-b-2 border-transparent hover:border-lightGrey duration-300"
           >
             <a
               href="/"
-              className="text-lightGrey text-sm md:py-5 py-3 inline-block"
+              className="text-lightGrey text-xl font-inter font-extrabold md:py-5 p-3 inline-block"
             >
               {link.name}
             </a>
           </li>
         ))}
+        <li className="p-5 ml-4">
+          <button className="text-lightGrey text-xs outline rounded-sm px-4 py-2">
+            Join Us
+          </button>
+        </li>
       </ul>
     </nav>
   )
