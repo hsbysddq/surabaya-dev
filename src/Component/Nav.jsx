e4155f84e0c98016624d8ba242c7b8dca17e2486
hsbysddq <88543652+hsbysddq@users.noreply.github.com>
2023-01-28 13:43:19 +0700

    Edit Nav

--- src/Component/Nav.jsx
+++ src/Component/Nav.jsx
@@ -3,8 +3,8 @@ import Logo from "../assets/logo.png"
 const Nav = () => {
   const Links = [{ name: "About" }, { name: "Event" }, { name: "Career" }]
   return (
-    <nav className="flex items-center justify-between pt-8">
-      <img src={Logo} alt="" className="w-44" />
+    <nav className="flex items-center justify-between pt-5 xl:pt-5 lg:pt-6 md:pt-4">
+      <img src={Logo} alt="" className="w-20 xl:w-42 lg:w-36 md:w-32" />
       <ul className="flex">
         {Links.map((link, index) => (
           <li
@@ -12,15 +12,15 @@ const Nav = () => {
             className="md:inline-block md:ml-10 ml-5 border-b-2 border-transparent hover:border-lightGrey duration-300"
           >
             <a
-              href="/"
-              className="text-lightGrey text-xl font-inter font-extrabold md:py-5 p-3 inline-block"
+              href="#about"
+              className="text-lightGrey text-sm lg:text-xl md:text-lg font-inter lg:font-extrabold md:font-bold md:py-5 lg:p-3 inline-block"
             >
               {link.name}
             </a>
           </li>
         ))}
-        <li className="p-5 ml-4">
-          <button className="text-lightGrey text-xs outline rounded-sm px-4 py-2">
+        <li className="lg:p-3 md:p-5 md:ml-4">
+          <button className="text-lightGrey font-inter text-sm md:text-xs outline rounded-sm px-2 lg:px-4 md:px-3 py-1 md:py-2">
             Join Us
           </button>
         </li>
