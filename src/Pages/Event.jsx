c68204bdf0d13d2d7e42e08df43797b1ba704b53
hsbysddq <88543652+hsbysddq@users.noreply.github.com>
2023-01-29 19:19:47 +0700

    Create Event

---
+++ src/Pages/Event.jsx
@@ -0,0 +1,14 @@
+import React from "react"
+import Card from "../Component/Card"
+
+const Event = () => {
+  return (
+    <div className="bg-about w-screen">
+      <div className="container">
+        <Card />
+      </div>
+    </div>
+  )
+}
+
+export default Event
