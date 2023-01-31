13033d4e1657ad4c866d649f3201b74af539248f
hsbysddq <88543652+hsbysddq@users.noreply.github.com>
2023-01-31 09:37:05 +0700

    add img

--- src/Component/Card.jsx
+++ src/Component/Card.jsx
@@ -1,16 +1,19 @@
-import Logo from "../assets/hero.png"
+import Logo from "../assets/3.jpeg"
 
 const Card = () => {
   return (
     <div className="card bg-white shadow-xl mb-16">
       <figure>
-        <img src={Logo} alt="Shoes" className="w-48" />
+        <img src={Logo} alt="Shoes" className="w-48 pt-8 rounded-[5%]" />
       </figure>
       <div className="card-body">
-        <h2 className="card-title">Shoes!</h2>
-        <p>If a dog chews shoes whose shoes does he choose?</p>
-        <div className="card-actions justify-end">
-          <button className="btn btn-primary">Buy Now</button>
+        <h2 className="card-title">React Js</h2>
+        <p>
+          SAVE DATE! 🗓 <br /> Sabtu, 17 Desember 2022 <br /> ⏰ 15.00 - 16.30
+          WIB 💻 <br />
+        </p>
+        <div className="card-actions justify-end pt-2">
+          <button className="btn btn-primary">Daftar</button>
         </div>
       </div>
     </div>
